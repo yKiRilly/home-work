9d1d773046100d38eed607c2e8b8c26d2f753cb2
yKiRilly <124667580+yKiRilly@users.noreply.github.com>
2024-03-17 21:53:53 +0100

    Add files via upload

---
+++ lesson15/main.js
@@ -0,0 +1,3 @@
+const age = '17' 
+
+age == '17' ? console.log ('Вам еще нет 18') : console.log ('Вы совершеннолетний')
---
+++ lesson16/main.js
@@ -0,0 +1,45 @@
+const chapter1 = 'First'
+
+console.log (chapter1)
+
+
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log (i)
+}
+
+/*--------------------------------------------------*/
+
+const chapter2 = 'Second'
+
+console.log (chapter2)
+
+
+for (let i = 10; i >= 1; i = i - 1) {
+    console.log (i)
+}
+
+/*--------------------------------------------------*/
+
+const chapter3 = 'Third'
+
+console.log (chapter3)
+
+let i = 2
+
+while(i <= 12) {
+    i = i + 2
+    console.log (i)
+}
+
+/*--------------------------------------------------*/
+
+const chapter4 = 'Fourth'
+
+console.log (chapter4)
+
+let i = 10
+
+while(i >= 1) {
+    i = i - 1
+    console.log (i)
+}
